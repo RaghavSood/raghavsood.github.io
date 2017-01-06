ae614e7bb4b996bb7118273e2deaa72731a30553
Raghav Sood <raghavsood@appaholics.in>
2017-01-07 02:49:50 +0800

    Added videos

--- new.js
+++ new.js
@@ -1,10 +1,16 @@
 $( document ).ready(function() {
     $(".button-collapse").sideNav();
-    $('.scrollspy').scrollSpy({scrollOffset: 50});
+    $('.scrollspy').scrollSpy({scrollOffset: 20});
     $('.experiencespy').scrollSpy({scrollOffset: 5});
     $('.experiencenav').pushpin({
       top: $('.experiencenav').offset().top - 15,
       bottom: $('#brilliant').offset().top + $('#brilliant').outerHeight() - $('.experiencenav').height(),
+      offset: 0
+    });
+    $('.talksspy').scrollSpy({scrollOffset: 5});
+    $('.talksnav').pushpin({
+      top: $('.talksnav').offset().top - 15,
+      bottom: $('#npc').offset().top + $('#npc').outerHeight() - $('.talksnav').height(),
       offset: 0
     });
 });
